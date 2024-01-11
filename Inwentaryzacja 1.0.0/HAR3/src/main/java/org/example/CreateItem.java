ab130720e1bf060e3c772b67cedc548f9915d412
OlekxDDD <101430843+OlekxDDD@users.noreply.github.com>
2024-01-11 09:12:08 +0100

    Dodanie poprawnosci zmiennych dodawanych do Bazy danych

--- Inwentaryzacja 1.0.0/HAR3/src/main/java/org/example/CreateItem.java
+++ Inwentaryzacja 1.0.0/HAR3/src/main/java/org/example/CreateItem.java
@@ -12,7 +12,7 @@ public class CreateItem extends JPanel implements ActionListener {
     private final JTextField NameField = new JTextField(); //field for Name
     private final JTextField DescriptionField = new JTextField();    //field for Description
     private final JTextField ValueField = new JTextField();  //field for value
-    private final JTextField AmountField = new JTextField(); //firld for amount
+    private final JTextField AmountField = new JTextField(); //field for amount
     private final JButton addButton; //add button
     final JButton WhatDoComeBackButton; //declarate What Do Come Back Button
     CreateItem(){
@@ -59,19 +59,62 @@ public class CreateItem extends JPanel implements ActionListener {
             Main.view = new View("WhatDo"); //back to What Do view
             Action.view.dispose();  //dispose this window
         }else if(source == addButton){//if click on Login button
-            String ID_K = "\""+ID_KField.getText()+"\"";
-            String name = "\""+NameField.getText()+"\"";
-            String description = "\""+DescriptionField.getText()+"\"";
-            String value = "\""+ ValueField.getText()+"\"";
-            String amount = "\""+AmountField.getText()+"\"";
+            String ID_KString = ID_KField.getText();    //declare variable for check their value
+            String nameString = NameField.getText();
+            String descriptionString = DescriptionField.getText();
+            String valueString =  ValueField.getText();
+            String amountString = AmountField.getText();
 
-            Action.addItem(ID_K,name,description,value,amount); //send this input to addItem
+            boolean isID_KString = Action.isDigitString(ID_KString);    // checking Strings are with only numbers/int
+            boolean isValueString = Action.isDigitString(valueString);
+            boolean isAmountString = Action.isDigitString(amountString);
 
-            ID_KField.setText(" ");
-            NameField.setText(" ");
-            DescriptionField.setText(" ");
-            ValueField.setText(" ");
-            AmountField.setText(" ");
+            boolean isLessThanMaxName = Action.isLessThanMaxText(nameString);   // checking Strings are Less than Max text
+            boolean isLessThanMaxDescription = Action.isLessThanMaxText(descriptionString);
+
+            boolean isID_KNull = !ID_KString.equals("");    //checking ID_k and Name aren't null
+            boolean isNameNull = !nameString.equals("");
+
+
+            if(isID_KString && isValueString && isAmountString && isLessThanMaxName && isLessThanMaxDescription && isID_KNull && isNameNull) {
+                String ID_K = "\"" + ID_KString + "\""; // add "" to Strings
+                String name = "\"" + nameString + "\"";
+                String description = "\"" + descriptionString + "\"";
+                String value = "\"" + valueString + "\"";
+                String amount = "\"" + amountString + "\"";
+
+                Action.addItem(ID_K, name, description, value, amount); //send this input to addItem
+
+                ID_KField.setText(" "); //clear input fields
+                NameField.setText(" ");
+                DescriptionField.setText(" ");
+                ValueField.setText(" ");
+                AmountField.setText(" ");
+            }else{
+                String MessegaToShowInAlert = "";   // declare variable for message
+                if(!isID_KString){
+                    MessegaToShowInAlert += "ID_K musi zawierac tylko liczby\n";
+                }
+                if(!isAmountString){
+                    MessegaToShowInAlert += "Ilosc musi zawierac tylko liczby\n";
+                }
+                if(!isValueString){
+                    MessegaToShowInAlert += "Wartosc musi zawierac tylko liczby\n";
+                }
+                if(!isLessThanMaxDescription){
+                    MessegaToShowInAlert += "Opis moze zawierac maksymalnie 65 535 znaków\n";
+                }
+                if(!isLessThanMaxName){
+                    MessegaToShowInAlert += "Nazwa moze zawierac maksymalnie 65 535 znaków\n";
+                }
+                if(!isID_KNull){
+                    MessegaToShowInAlert += "ID_K nie moze byc pusty\n";
+                }
+                if(!isNameNull){
+                    MessegaToShowInAlert += "Name nie moze byc pusty\n";
+                }
+                JOptionPane.showMessageDialog(null, MessegaToShowInAlert);  // show message ot user
+            }
         }
     }
 }
